6258ba726c4844f7a337d5a71631dbe16082a4cb
Ntropy-86 <nitigya.199301207@muj.manipal.edu>
2022-07-12 16:01:16 +0530

    ⚙️ -  Dynamic Components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+// import HelloWorld from "components/apps/HelloWorld";
 import type { NextPage } from "next";
 import Head from "next/head";
+import processDirectory from "utils/processDirectory";
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +12,8 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-black text-5xl text-center">Hello World</h1>
+        {/* <h1 className="text-black text-5xl text-center">Hello World</h1> */}
+        <processDirectory.HelloWorld.Component />
       </main>
     </div>
   );
