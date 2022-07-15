28d36fa44ecedc9b3dcb80109a875370a42b50f4
Ntropy-86 <nitigya.199301207@muj.manipal.edu>
2022-07-15 19:37:48 +0530

    ⚙️ -  Jest Config Refactor

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   moduleDirectories: ["<rootDir>", "node_modules"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect",
+    "<rootDir>/jest.setup.js",
+  ],
   testEnvironment: "jest-environment-jsdom",
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,4 @@
-import "@testing-library/jest-dom/extend-expect";
-
-// This will mock `next/dynamic`, so make sure to import it before any of your
-// components.
 import preloadAll from "jest-next-dynamic";
-// This component can have dynamic imports anywhere in its rendered tree,
-// including nested dynamic imports.
 
 beforeAll(async () => {
   await preloadAll();
